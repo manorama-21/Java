488cb8071fef41321a6b9e37b46c79a09696f348
MANORAMA <122603231+manorama-21@users.noreply.github.com>
2024-05-29 20:28:36 +0530

    Update method.java

--- Self study/method.java
+++ Self study/method.java
@@ -11,7 +11,7 @@
 
 public class method {
     static void myMethod() {
-      System.out.println("I am done");
+      System.out.println("My first method");
     }
   
     public static void main(String[] args) {
